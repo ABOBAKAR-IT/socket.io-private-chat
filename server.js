94524d1eff538795b683ff94dfd5fe6df6a22ea8
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-01-23 08:35:07 +0500

    done

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const database=require('./4_model/database')
-const userRoute=require('./1_router/userRouter')
 const PORT = process.env.PORT || 3110
 http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 app.use(express.static(__dirname + '/public'))
-app.use('/user',userRoute)
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-
 // Socket 
 const io = require('socket.io')(http)
 var connectedUsers = {};
 var userbox = {}
-
 io.on('connection', (socket) => {
   console.log('Connected...')
   console.log(socket.id);
